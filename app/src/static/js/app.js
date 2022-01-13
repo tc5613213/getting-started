17692c7096da4828b81b0b12335449bfb0d11cc7
tc5613213 <56156004+tc5613213@users.noreply.github.com>
2022-01-13 11:20:45 +1100

    Update

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add'}
+                        {submitting ? 'Adding...' : 'Nathan: Add'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
